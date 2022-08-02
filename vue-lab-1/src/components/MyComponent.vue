b2c5201261f393d26f0f82eb9c06a543cbb59b5d
MarianM <marianmelinte90@gmail.com>
2022-08-02 20:04:04 +0200

    vue setup

---
+++ vue-lab-1/src/components/MyComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>My Component</h2>
+    <p>El numero es {{ numero }}</p>
+    <button @click="sumar"> Sumar </button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            numero: 0
+        };
+    },
+    methods: {
+        sumar() {
+            this.numero ++;
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ vue-lab-1/src/components/ToDoItem.vue
@@ -0,0 +1,38 @@
+<template> 
+    <div id="todo-item">
+        <span :style="color">To Do Item</span>
+        <button @click="done">DONE</button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    done () {
+      this.color = 'color:blue'
+    }
+  },
+
+  data() {
+    return {
+      color: 'color:red'
+    }
+  }
+  
+}
+</script>
+
+<style>
+#todo-list {
+  text-align: left;
+}
+
+  #todo-item {
+    color:blue !important
+  }
+
+ li {
+  list-style: none;
+ }
+
+</style>
